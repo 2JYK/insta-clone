f32bfa1937c63cc95bbc4d67753d9ed6183ab255
GyeongSuKim <rla04722@naver.com>
2022-05-03 15:27:43 +0900

    Delete

--- daegeun.py
+++
@@ -1,6 +0,0 @@
-print("hello world!")
-print("this is my commit practice!")
-
-print("this is my commit practice!")
-print("this is my commit practice!")
-print("this is my commit practice!")
--- test_jh.py
+++
@@ -1,9 +0,0 @@
-def laundry():
-    print('빨래하기 싫다')
-
-laundry()
-
-print('주현' + laundry())
-
-
-print('ju hi what plan bab memu?')
