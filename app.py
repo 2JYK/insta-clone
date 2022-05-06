39ef8e7ad4ffbc8bdac173b6e3a7fab79bb47168
GyeongSuKim <rla04722@naver.com>
2022-05-06 16:46:23 +0900

    Feat : 유저 정보 확인 후 토큰 발급

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def home():
 		# 암호화되어있는 token의 값을 우리가 사용할 수 있도록 디코딩(암호화 풀기)해줌
 		payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 		user_info = db.user_info.find_one({"id": payload['id']})
-		return render_template('index.html', nickname=user_info["nick"])
+		return render_template('index.html', user_name=user_info["name"])
 	# 만약 해당 token의 로그인 시간이 만료되었다면, 아래와 같은 코드를 실행.
 	except jwt.ExpiredSignatureError:
 		return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -124,6 +124,36 @@ def api_login():
 	# 찾지 못하면
 	else:
 		return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+
+
+# [유저 정보 확인 API]
+# 로그인된 유저만 call 할 수 있는 API입니다.
+# 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
+# (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
+@app.route('/name', methods=['GET'])
+def api_valid():
+	token_receive = request.cookies.get('mytoken')
+
+	# try / catch 문?
+	# try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
+
+	try:
+		# token을 시크릿키로 디코딩.
+		# 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
+		payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+		print(payload)
+
+		# payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
+		# 여기에선 그 예로 닉네임을 보내주겠습니다.
+		userinfo = db.user_info.find_one({'id': payload['id']}, {'_id': 0})
+		return jsonify({'result': 'success', 'user_name': userinfo['name']})
+	except jwt.ExpiredSignatureError:
+		# 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+		return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+	except jwt.exceptions.DecodeError:
+		return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
 
