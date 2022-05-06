2603c05f404a27090a0367380252955c05e6b12f
juhyun1011 <juhyun4567@gmail.com>
2022-05-06 11:56:53 +0900

    Feat : Mypage Modal

---
+++ static/js/mypage-jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('#modal_open').click(function() {
+        $('#modal').show();
+    })
+    $("#modal_close").click(function () {
+      $('#modal').hide();
+    })
+
+     $('#modal_open2').click(function() {
+        $('#modal2').show();
+    })
+    $("#modal_close2").click(function () {
+      $('#modal2').hide();
+    })
+
+})
